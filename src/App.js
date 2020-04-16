58b76c4e67ec5ab35682c1878c17978fd7a3358a
Saurabh Kumar <dsaurabh.kumar1288@gmail.com>
2020-04-16 13:28:41 +0530

    Working code of CRUD and Routing in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AddUser from './components/users/AddUser';
+import EditUser from './components/users/EditUser';
+import ViewUsers from './components/users/ViewUsers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/users/add' component={AddUser}></Route>
+          <Route exact path='/users/edit/:id' component={EditUser}></Route>
+          <Route exact path='/users/:id' component={ViewUsers}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
